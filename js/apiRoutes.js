f5fb203e684f0f8f906d977c5591b72ac42a48b8
betheng <131614496+betheng@users.noreply.github.com>
2023-07-23 18:30:28 -0400

    install express and add api routes

---
+++ js/apiRoutes.js
@@ -0,0 +1,53 @@
+// required filesnpm init
+const fs = require("fs");
+const util = require("util");
+const app = require("express").Router();
+const writeFileAsync = util.promisify(fs.writeFile);
+const readFileAsync = util.promisify(fs.readFile);
+var notesDataFormat;
+
+// read contents of the "db.json" file, parse it as JSON, send JSON data back to client as a response
+app.get("/notes", (req, res) => {
+  readFileAsync("db/db.json", "utf8").then(function (data) {
+    notesDataFormat = JSON.parse(data);
+    res.json(notesDataFormat);
+  });
+});
+
+// read existing notes from "db.json" file, add new note to the list, write updated list of notes back to file
+app.post("/notes", (req, res) => {
+  readFileAsync("db/db.json", "utf8").then(function (data) {
+    notesDataFormat = JSON.parse(data);
+
+    let newNote = req.body;
+    let currentID = notesDataFormat.length;
+
+    newNote.id = currentID + 1;
+    notesDataFormat.push(newNote);
+
+    notesDataFormat = JSON.stringify(notesDataFormat);
+
+    writeFileAsync("db/db.json", notesDataFormat).then(function (data) {
+      console.log("Note has been added.");
+    });
+    res.json(notesDataFormat);
+  });
+});
+
+// search for note ID in the notesDataFormat array, delete it from the array, update the "db.json" file with the modified array, respond with the updated array of notes
+app.delete("/notes/:id", (req, res) => {
+  let selID = parseInt(req.params.id);
+  for (let i = 0; i < notesDataFormat.length; i++) {
+    if (selID === notesDataFormat[i].id) {
+      notesDataFormat.splice(i, 1);
+      let noteJSON = JSON.stringify(notesDataFormat, null, 2);
+
+      writeFileAsync("db/db.json", noteJSON).then(function () {
+        console.log("Note has been deleted.");
+      });
+    }
+  }
+  res.json(notesDataFormat);
+});
+
+module.exports = api;
